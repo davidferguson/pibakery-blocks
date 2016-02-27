0e0050519f0016e5b7d183db0d0e0de31da59f8e
David Ferguson <davidferguson@users.noreply.github.com>
2016-02-27 22:04:51 +0000

    initial commit of wifisetup block

---
+++ wifisetup/wifiConnect.py
@@ -0,0 +1,19 @@
+import sys, os
+
+wifiText = """
+
+network={
+    ssid="WIFI-SSID"
+    psk="WIFI-PSK"
+}"""
+
+wifiSSID = sys.argv[1]
+wifiPSK = sys.argv[2]
+
+wifiText.replace("WIFI-SSID", wifiSSID).replace("WIFI-PSK", wifiPSK)
+
+with open("/etc/wpa_supplicant/wpa_supplicant.conf", "a") as wifiFile:
+	wifiFile.write(wifiText)
+
+os.system("sudo ifdown wlan0")
+os.system("sudo ifup wlan0")
