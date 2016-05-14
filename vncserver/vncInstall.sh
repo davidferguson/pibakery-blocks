6c95d3c81837745c3c18fecf31872ab1b14587f9
David Ferguson <David@Davids-MacBook-Pro.local>
2016-05-14 16:11:04 +0100

    removed now unnecessary expandsd and fixed vncserver

--- vncserver/vncInstall.sh
+++ vncserver/vncInstall.sh
@@ -1,6 +1,6 @@
 apt-get update
 #DEBIAN_FRONTEND=noninteractive apt-get -y install tightvncserver
-dpkg -i /boot/PiBakery/vncserver/xfonts-base_1.0.3_all.deb /boot/PiBakery/vncserver/tightvncserver_1.3.9-6.5_armhf.deb
+dpkg -i /boot/PiBakery/blocks/vncserver/xfonts-base_1.0.3_all.deb /boot/PiBakery/blocks/vncserver/tightvncserver_1.3.9-6.5_armhf.deb
 
 chmod +x /boot/PiBakery/blocks/vncserver/vncSetup.sh
 su - pi -c "/boot/PiBakery/blocks/vncserver/vncSetup.sh $@"
--- vncserver/vncSetup.sh
+++ vncserver/vncSetup.sh
@@ -2,3 +2,4 @@ umask 0077
 mkdir -p "/home/pi/.vnc"
 chmod go-rwx "/home/pi/.vnc"
 vncpasswd -f <<<"$1" >"/home/pi/.vnc/passwd"
+vncserver :1
