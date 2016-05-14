e45c1c35e319aba6352be8eab1de29ccf2d877b6
David Ferguson <davidferguson@users.noreply.github.com>
2016-05-14 14:53:10 +0100

    corrected paths of deb files

--- vncserver/vncInstall.sh
+++ vncserver/vncInstall.sh
@@ -1,6 +1,6 @@
 apt-get update
 #DEBIAN_FRONTEND=noninteractive apt-get -y install tightvncserver
-dpkg -i xfonts-base_1.0.3_all.deb tightvncserver_1.3.9-6.5_armhf.deb
+dpkg -i /boot/PiBakery/vncserver/xfonts-base_1.0.3_all.deb /boot/PiBakery/vncserver/tightvncserver_1.3.9-6.5_armhf.deb
 
 chmod +x /boot/PiBakery/blocks/vncserver/vncSetup.sh
 su - pi -c "/boot/PiBakery/blocks/vncserver/vncSetup.sh $@"
