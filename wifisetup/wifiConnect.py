5dfd7c829d331a3960d221507fbefb2905f10209
David Ferguson <davidferguson@users.noreply.github.com>
2016-05-25 13:22:49 +0100

    added support for open and wep networks

--- wifisetup/wifiConnect.py
+++ wifisetup/wifiConnect.py
@@ -1,33 +1,52 @@
 #!/usr/bin/env python
 
-import sys, os
-
-wpaWifi = """
-
-network={
-    ssid="WIFI-SSID"
-    psk="WIFI-PSK"
-}"""
+import sys, os, time
 
 openWifi = """
 
 network={
     ssid="WIFI-SSID"
+    scan_ssid=1
     key_mgmt=NONE
+}"""
+
+wepWifi = """
+
+network={
+    ssid="WIFI-SSID"
+    scan_ssid=1
+    key_mgmt=NONE
+    wep_key0="WIFI-PSK"
+}"""
+
+wpaWifi = """
+
+network={
+    ssid="WIFI-SSID"
+    scan_ssid=1
+    key_mgmt=WPA-PSK
+    psk="WIFI-PSK"
 }"""
 
 wifiSSID = sys.argv[1]
 wifiPSK = sys.argv[2]
+wifiType = sys.argv[3]
 
-if wifiSSID != "":
-	if wifiPSK != "":
-		wifiText = wpaWifi.replace("WIFI-SSID", wifiSSID).replace("WIFI-PSK", wifiPSK)
-	else:
+if wifiSSID != "" and wifiType != "":
+	if wifiPSK == "" or wifiType == "Open (no password)":
 		wifiText = openWifi.replace("WIFI-SSID", wifiSSID)
+	elif wifiType == "WEP":
+		wifiText = wepWifi.replace("WIFI-SSID", wifiSSID).replace("WIFI-PSK", wifiPSK)
+	elif wifiType == "WPA/WPA2":
+		wifiText = wpaWifi.replace("WIFI-SSID", wifiSSID).replace("WIFI-PSK", wifiPSK)
 
-os.system("killall wpa_supplicant")
 with open("/etc/wpa_supplicant/wpa_supplicant.conf", "a") as wifiFile:
 	wifiFile.write(wifiText)
 
-os.system("/sbin/wpa_supplicant -s -B -P /run/wpa_supplicant.wlan0.pid -i wlan0 -D nl80211,wext -c /etc/wpa_supplicant/wpa_supplicant.conf")
-os.system("dhclient wlan0")
+os.system("wpa_cli reconfigure")
+
+#It's likely that the block following this one will be one that uses the internet - such
+#as a download file or apt-get block. It takes a few seconds for the WiFi to connect and
+#obtain an IP address, so wat 30 seconds to make sure that if we can connect, we will
+#have
+time.sleep(30)
