75b6631a493ec49ca2a01317dc5ea26f6ce545e5
dsbert <danbertolini@gmail.com>
2016-09-24 19:16:10 -0400

    Add block to install docker

---
+++ docker/docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -sSL https://downloads.hypriot.com/docker-hypriot_1.10.3-1_armhf.deb >/tmp/docker-hypriot_1.10.3-1_armhf.deb
+dpkg -i /tmp/docker-hypriot_1.10.3-1_armhf.deb
+rm -f /tmp/docker-hypriot_1.10.3-1_armhf.deb
+usermod -aG docker pi
+systemctl enable docker.service
