fefe7708487f2fb0eb867cdf5f76cc4433ec234d
Matheus Dal Mago <matheusdalmago10@hotmail.com>
2016-11-20 14:41:02 -0200

    adding block pipinstall

---
+++ pipinstall/pipinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get update
+
+if [ "$2" == "pip" ] then
+  DEBIAN_FRONTEND=noninteractive apt-get install -y python-pip
+  pip install "$1"
+else
+  DEBIAN_FRONTEND=noninteractive apt-get install -y python3-pip
+  pip3 install "$1"
+fi
