23e6857fa9903c23dd55c804a19ad0525ea4a689
Ruud Boon <ruud@ruudboon.io>
2017-01-24 17:29:27 +0100

    fixed issues with crashing info

--- setkiosk/setkiosk.sh
+++ setkiosk/setkiosk.sh
@@ -5,8 +5,7 @@ text="@lxpanel --profile LXDE-pi
 @xset s off
 @xset -dpms
 @xset s noblank
-@sed -i 's/\"exited_cleanly\": false/\"exited_cleanly\": true/' /home/pi/.config/chromium/Default/Preferences
-@chromium-browser --noerrdialogs --kiosk --disable-session-crashed-bubble -no-first-run "$1"
+@chromium-browser --noerrdialogs --kiosk --disable-session-crashed-bubble --disable-infobars -no-first-run "$1"
 @unclutter -idle 0.1 -root"
 
 echo "$text" > /home/pi/.config/lxsession/LXDE-pi/autostart
