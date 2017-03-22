83f5119d4141c455fde8a73a87c3a15c83b0228b
eddwatts <edd@eddtech.co.uk>
2017-03-22 15:15:37 +0000

    Update powerdown17.sh

--- powerdown17/powerdown17.sh
+++ powerdown17/powerdown17.sh
@@ -2,6 +2,6 @@
 mkdir /home/pi/powerdown
 cp /boot/PiBakery/blocks/powerdown17/powerdown.py /home/pi/powerdown/powerdown.py
 su - root -c cp /boot/PiBakery/blocks/powerdown17/powerdown.service /etc/systemd/system/powerdown.service
-su - root -c chmod +x /home/pi/lipopi/powerdown.py
+su - root -c chmod +x /home/pi/powerdown/powerdown.py
 su - root -c systemctl enable powerdown.service
 su - root -c systemctl start  powerdown.service
