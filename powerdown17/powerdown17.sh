4abd08b9754aee53bd7a159d8730e3ff59368f23
eddwatts <edd@eddtech.co.uk>
2017-03-24 13:26:32 +0000

    Update powerdown17.sh

--- powerdown17/powerdown17.sh
+++ powerdown17/powerdown17.sh
@@ -6,3 +6,4 @@ su - root -c chmod +x /home/pi/powerdown/powerdown.py
 su - root -c systemctl enable powerdown.service
 su - root -c systemctl start  powerdown.service
 sed -i 's/=17/='$1'/' /home/pi/powerdown/powerdown.py
+sed -i 's/=18/='$2'/' /home/pi/powerdown/powerdown.py
