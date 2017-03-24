80f0f39b169c26b5ad118cf282434319d2d78bd9
eddwatts <edd@eddtech.co.uk>
2017-03-24 14:08:55 +0000

    Update powerdown17.sh

--- powerdown17/powerdown17.sh
+++ powerdown17/powerdown17.sh
@@ -5,5 +5,5 @@ su - root -c cp /boot/PiBakery/blocks/powerdown17/powerdown.service /etc/systemd
 su - root -c chmod +x /home/pi/powerdown/powerdown.py
 su - root -c systemctl enable powerdown.service
 su - root -c systemctl start  powerdown.service
-sed -i 's/=17/='$1'/' /home/pi/powerdown/powerdown.py
-sed -i 's/=18/='$2'/' /home/pi/powerdown/powerdown.py
+sed -i 's/=XXPIN/='$1'/' /home/pi/powerdown/powerdown.py
+sed -i 's/=YYPIN/='$2'/' /home/pi/powerdown/powerdown.py
