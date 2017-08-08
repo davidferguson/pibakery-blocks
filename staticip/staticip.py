8595bddf19b23e118dc39e49e9e867764c6f8cf7
jrm86 <jrm86git@gmail.com>
2017-08-08 07:14:50 -0400

    Update staticip.py

--- staticip/staticip.py
+++ staticip/staticip.py
@@ -3,40 +3,30 @@
 import sys, os, time
 
 setWiFi = """
-
 interface wlan0
-
 static ip_address="STATIC-IP"
 static routers="ROUTER-IP"
 static domain_name_servers="DNS-IP"
-
 }"""
 
 setEth = """
-
 interface eth0
-
 static ip_address="STATIC-IP"
 static routers="ROUTER-IP"
 static domain_name_servers="DNS-IP"
-
 }"""
 
 setBoth = """
-
 interface eth0
-
 static ip_address="STATIC-IP"
 static routers="ROUTER-IP"
 static domain_name_servers="DNS-IP"
-
 interface wlan0
-
 static ip_address="STATIC-IP"
 static routers="ROUTER-IP"
 static domain_name_servers="DNS-IP"
-
 }"""
+
 
 staticIP = sys.argv[1]
 routerIP = sys.argv[2]
@@ -44,20 +34,16 @@ dnsIP = sys.argv[3]
 type = sys.argv[4]
 
 
-
-if type = "WiFi":
-
+if type == "WiFi":
 	ipText = setWiFi.replace("STATIC-IP", staticIP).replace("ROUTER-IP", routerIP).replace("DNS-IP", dnsIP)
-	elif type = "Ethernet":
-		ipText = setEth.replace("STATIC-IP", staticIP).replace("ROUTER-IP", routerIP).replace("DNS-IP", dnsIP)
-	else:
-		ipText = setBoth.replace("STATIC-IP", staticIP).replace("ROUTER-IP", routerIP).replace("DNS-IP", dnsIP)
-		
+elif type == "Ethernet":
+	ipText = setEth.replace("STATIC-IP", staticIP).replace("ROUTER-IP", routerIP).replace("DNS-IP", dnsIP)
+else:
+	ipText = setBoth.replace("STATIC-IP", staticIP).replace("ROUTER-IP", routerIP).replace("DNS-IP", dnsIP)
+
+  
 with open("/etc/dhcpcd.conf", "a") as ipFile:
 	ipFile.write(ipText)
 
-	
-os.system("Configuring Static IP")
+ 
 time.sleep(5)
-
-
