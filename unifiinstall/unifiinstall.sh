8dc00b3420455b2c4e5fd44e4ef31523ea189a5e
eddwatts <edd@eddtech.co.uk>
2017-10-16 16:14:20 +0100

    option for version and updated key, add mongodb

--- unifiinstall/unifiinstall.sh
+++ unifiinstall/unifiinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'deb http://www.ubnt.com/downloads/unifi/debian unifi5 ubiquiti' | sudo tee -a /etc/apt/sources.list.d/ubnt.list > /dev/null
-apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50
+echo 'deb http://www.ubnt.com/downloads/unifi/debian $1 ubiquiti' | sudo tee -a /etc/apt/sources.list.d/ubnt.list > /dev/null
+apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
 apt-get update
-apt-get install unifi oracle-java8-jdk -y
+apt-get install unifi oracle-java8-jdk mongodb -y
 echo 'ENABLE_MONGODB=no' | sudo tee -a /etc/mongodb.conf > /dev/null
 cp /lib/systemd/system/unifi.service /etc/systemd/system/
 sed -i '/^\[Service\]$/a Environment=JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt' /etc/systemd/system/unifi.service
